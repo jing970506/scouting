149583adc236fdf7b5cdc106367c53d7f3f994fd
jing970506 <jing.t.0506@gmail.com>
2024-05-29 23:07:37 +0800

    5/29 進度感人

--- scouting_Jing.py
+++ scouting_Jing.py
@@ -8,3 +8,20 @@ sheet=account.open("open")
 all=sheet.worksheet("All")
 
 team=None
+def data(inf):
+    line = inf.acell("Q1").value
+    inf.update('A'+line,all.acell("B2").value)
+    inf.update('C'+line,int(all.acell("D2").value))
+    inf.update('D'+line,all.acell("E2").value)
+    inf.update('E'+line,all.acell("F2").value)
+    inf.update('G'+line,all.acell("H2").value)
+    inf.update('H'+line,int(all.acell("I2").value))
+    inf.update('I'+line,int(all.acell("J2").value))
+    inf.update('J'+line,int(all.acell("K2").value))
+    inf.update('K'+line,int(all.acell("L2").value))
+    inf.update('L'+line,all.acell("M2").value)
+    inf.update('M'+line,all.acell("N2").value)
+    inf.update('N'+line,int(all.acell("O2").value))
+    inf.update('O'+line,all.acell("P2").value)
+    inf.update('P'+line,all.acell("Q2").value)
+    inf.update('Q1',int(l)+1)
