462dcefbc955b482fbcc2e78b08e6837d2fefc5b
jing970506 <jing.t.0506@gmail.com>
2024-05-31 16:10:54 +0800

    5/31應該算是搞定了?

--- scouting_Jing.py
+++ scouting_Jing.py
@@ -3,7 +3,7 @@ import gspread
 import time
 file_path=pathlib.Path(__file__)
 json_path=pathlib.Path(file_path).parent/"scouting-jing-4c9747d8a2c5.json"
-account=gspread.servise_account(json_path)
+account=gspread.service_account(json_path)
 sheet=account.open("scouting")
 all=sheet.worksheet("All")
 
